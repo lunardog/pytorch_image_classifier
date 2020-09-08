90f0a58653f13ee61476221a7aa56d62979698cd
lunardog <lunardog@icloud.com>
2020-09-08 20:15:22 +0900

    upgrade torch

--- train.py
+++ train.py
@@ -20,26 +20,28 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.autograd import Variable
-import numpy as np
+
+# import numpy as np
 import torchvision
 from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
+from torch.optim import lr_scheduler
+
+# import matplotlib.pyplot as plt
 import time
 import copy
-import os
-from global_config import *
+from global_config import DEVICE, IMAGE_SIZE, MODEL_SAVE_FILE
 from data_loader.data_loader import DataLoader
 from models.fine_tune_model import fine_tune_model
 
 
-def train_model(data_loader, model, criterion, optimizer, lr_scheduler, num_epochs=25):
+def train_model(data_loader, model, criterion, optimizer, scheduler, num_epochs=25):
     """
     the pipeline of train PyTorch model
     :param data_loader:
     :param model:
     :param criterion:
     :param optimizer:
-    :param lr_scheduler:
+    :param scheduler:
     :param num_epochs:
     :return:
     """
@@ -49,67 +51,71 @@ def train_model(data_loader, model, criterion, optimizer, lr_scheduler, num_epoc
     best_acc = 0
 
     for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
+        print("Epoch {}/{}".format(epoch, num_epochs - 1))
+        print("-" * 10)
 
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                optimizer = lr_scheduler(optimizer, epoch)
-                model.train(True)  # Set model to training mode
+        for phase in ["train", "val"]:
+            if phase == "train":
+                model.train()  # Set model to training mode
             else:
-                model.train(False)
+                model.eval()
 
             running_loss = 0.0
             running_corrects = 0
 
-            for batch_data in data_loader.load_data(data_set=phase):
-                inputs, labels = batch_data
-                if USE_GPU:
-                    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
+            for inputs, labels in data_loader.load_data(data_set=phase):
+                inputs, labels = inputs.to(DEVICE), labels.to(DEVICE)
 
                 optimizer.zero_grad()
 
-                outputs = model(inputs)
-                _, predict = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
+                with torch.set_grad_enabled(phase == "train"):
+
+                    outputs = model(inputs)
+                    _, predict = torch.max(outputs.data, 1)
+                    loss = criterion(outputs, labels)
+                    if phase == "train":
+                        loss.backward()
+                        optimizer.step()
 
                 # collect data info
-                running_loss += loss.data[0]
+                running_loss += loss.item() + inputs.size(0)
                 running_corrects += torch.sum(predict == labels.data)
 
+            if phase == "train":
+                scheduler.step()
+
             epoch_loss = running_loss / data_loader.data_sizes[phase]
-            epoch_acc = running_corrects / data_loader.data_sizes[phase]
+            epoch_acc = running_corrects / float(data_loader.data_sizes[phase])
+            print(running_corrects)
+            print(data_loader.data_sizes)
 
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
+            print("{} Loss: {:.4f} Acc: {:.4f}".format(phase, epoch_loss, epoch_acc))
 
-            if phase == 'val' and epoch_acc > best_acc:
+            if phase == "val" and epoch_acc > best_acc:
                 best_acc = epoch_acc
                 best_model = copy.deepcopy(model)
 
         print()
 
     time_elapsed = time.time() - since_time
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print(
+        "Training complete in {:.0f}m {:.0f}s".format(
+            time_elapsed // 60, time_elapsed % 60
+        )
+    )
+    print("Best val Acc: {:4f}".format(best_acc))
     return best_model
 
 
 def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
-    lr = init_lr * (0.1**(epoch // lr_decay_epoch))
+    lr = init_lr * (0.1 ** (epoch // lr_decay_epoch))
 
     if epoch % lr_decay_epoch == 0:
-        print('LR is set to {}'.format(lr))
+        print("LR is set to {}".format(lr))
 
     for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+        param_group["lr"] = lr
 
     return optimizer
 
@@ -119,26 +125,32 @@ def save_torch_model(model, name):
 
 
 def train():
-    data_loader = DataLoader(data_dir='datasets/hymenoptera_data', image_size=IMAGE_SIZE, batch_size=4)
+    data_loader = DataLoader(
+        data_dir="datasets/hymenoptera_data", image_size=IMAGE_SIZE, batch_size=4
+    )
     inputs, classes = next(iter(data_loader.load_data()))
     out = torchvision.utils.make_grid(inputs)
-    data_loader.show_image(out, title=[data_loader.data_classes[c] for c in classes])
+    # data_loader.show_image(out, title=[data_loader.data_classes[c] for c in classes])
 
-    # model = fine_tune_model()
-    #
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    # try:
-    #     model = train_model(data_loader, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
-    #     save_torch_model(model, MODEL_SAVE_FILE)
-    # except KeyboardInterrupt:
-    #     print('manually interrupt, try saving model for now...')
-    #     save_torch_model(model, MODEL_SAVE_FILE)
-    #     print('model saved.')
+    model = fine_tune_model()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+    try:
+        model = train_model(
+            data_loader, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25
+        )
+        save_torch_model(model, MODEL_SAVE_FILE)
+    except KeyboardInterrupt:
+        print("manually interrupt, try saving model for now...")
+        save_torch_model(model, MODEL_SAVE_FILE)
+        print("model saved.")
 
 
 def main():
     train()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
