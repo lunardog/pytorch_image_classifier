c47cb9e2e37f9f909177c37b1d61d0e16c4bbaf4
lunardog <lunardog@icloud.com>
2020-09-08 20:13:58 +0900

    add DEVICE to global config

--- global_config.py
+++ global_config.py
@@ -22,4 +22,5 @@ import torch
 IMAGE_SIZE = 244
 
 USE_GPU = torch.cuda.is_available()
-MODEL_SAVE_FILE = 'ants_and_bees.pth'
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+MODEL_SAVE_FILE = "ants_and_bees.pth"
